6ab0f410d6ef4ad09fe2bf47590d408de5b91360
jmcollongette <jmcollongette@oxya.com>
2018-02-15 16:52:09 +0100

    changing DEFAULT_MAX_FEEDRATE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -536,7 +536,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 400, 2, 45 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
